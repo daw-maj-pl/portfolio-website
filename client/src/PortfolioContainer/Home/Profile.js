8147f55b4b8c9b8a4d1aeddbe2f371d4dcfd1b98
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-11-01 12:20:46 +0100

    Add profile picture

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -7,18 +7,20 @@ const Profile = () => {
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
-            <a href="https://www.facebook.com/">
-              <i className="fa fa-facebook-square"></i>
-            </a>
-            <a href="https://www.instagram.com/">
-              <i className="fa fa-instagram"></i>
-            </a>
-            <a href="https://www.youtube.com/">
-              <i className="fa fa-youtube-square"></i>
-            </a>
-            <a href="https://twitter.com/">
-              <i className="fa fa-twitter"></i>
-            </a>
+            <div className="colz-icon">
+              <a href="https://www.facebook.com/">
+                <i className="fa fa-facebook-square"></i>
+              </a>
+              <a href="https://www.instagram.com/">
+                <i className="fa fa-instagram"></i>
+              </a>
+              <a href="https://www.youtube.com/">
+                <i className="fa fa-youtube-square"></i>
+              </a>
+              <a href="https://twitter.com/">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </div>
           </div>
           <div className="profile-details-name">
             <span className="primary-text">
@@ -55,6 +57,9 @@ const Profile = () => {
               <button className="btn highlighted-btn">Get Resume</button>
             </a>
           </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
         </div>
       </div>
     </div>
