8a420c6b6aa3c4d5e84f56ae1a89d76dace819ec
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-11-01 14:37:43 +0100

    Add Footer component and some styling to it

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import Profile from './PortfolioContainer/Home/Profile';
+import Footer from './PortfolioContainer/Home/Footer/Footer';
 import './App.css';
 
 const App = () => {
   return (
     <div>
       <Profile />
+      <Footer />
     </div>
   );
 };
